056eece210a560fd5ca0aff623d11edfae54b58c
Adrien N. F <iamnautilusjungle@gmail.com>
2023-10-12 23:14:01 -0400

    uncommenting important code

--- facades/StorageFacade.ts
+++ facades/StorageFacade.ts
@@ -2,18 +2,17 @@ import { MMKV } from 'react-native-mmkv'
 import { Image } from '../types/Image';
 
 export default class StorageFacade {
-	// protected static instance = new MMKV();
+	protected static instance = new MMKV();
 
 	protected static key = "images";
 
 	public static load(): Image[] {
-		return [];
-		// const images = this.instance.getString(this.key);
+		const images = this.instance.getString(this.key);
 
-		// return images ? JSON.parse(images) : [];
+		return images ? JSON.parse(images) : [];
 	}
 
 	public static save(images: Image[]): void {
-		// this.instance.set(this.key, JSON.stringify(images));
+		this.instance.set(this.key, JSON.stringify(images));
 	}
 }
